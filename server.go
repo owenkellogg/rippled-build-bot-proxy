28d42678bbc0d08eefda36baee76fd4ae03d6805
Steven Zeiler <zeiler.steven@gmail.com>
2015-10-01 14:39:22 -0700

    [TASK] go fmt

--- server.go
+++ server.go
@@ -1,42 +1,41 @@
 package main
 
-import(
-  "github.com/julienschmidt/httprouter"
-  "github.com/parnurzeal/gorequest"
-  "net/http"
-  "crypto/tls"
-  "fmt"
-  "log"
-  "io/ioutil"
-  "os"
+import (
+	"crypto/tls"
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"github.com/parnurzeal/gorequest"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
 )
 
 var (
-  certFile = "./server.crt"
-) 
+	certFile = "./server.crt"
+)
 
 func GithubWebhook(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
-  
-  cert, _ := tls.LoadX509KeyPair(certFile, "")
-  tlsConfig := &tls.Config{Certificates: []tls.Certificate{cert}, InsecureSkipVerify: true}
 
-  request := gorequest.New().TLSClientConfig(tlsConfig)
+	cert, _ := tls.LoadX509KeyPair(certFile, "")
+	tlsConfig := &tls.Config{Certificates: []tls.Certificate{cert}, InsecureSkipVerify: true}
 
-  url := "https://54.254.176.227/github"
+	request := gorequest.New().TLSClientConfig(tlsConfig)
 
-  requestBody, _ := ioutil.ReadAll(req.Body)
+	url := "https://54.254.176.227/github"
 
-  request.Post(url).
-    Send(string(requestBody)).
-    End()
+	requestBody, _ := ioutil.ReadAll(req.Body)
 
-  fmt.Fprintf(w, "Success!\n")
+	request.Post(url).
+		Send(string(requestBody)).
+		End()
+
+	fmt.Fprintf(w, "Success!\n")
 }
 
 func main() {
-  router := httprouter.New()
-  router.POST("/github", GithubWebhook)
+	router := httprouter.New()
+	router.POST("/github", GithubWebhook)
 
-  log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), router))
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), router))
 }
-
